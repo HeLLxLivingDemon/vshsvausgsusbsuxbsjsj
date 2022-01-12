71b524a61b3d1943c6a9ec144a19d54412a7cd59
HeLLxGodLike <ysonu4951@gmail.com>
2022-01-13 01:46:38 +0530

    Update decorators.py

--- oda/utils/decorators.py
+++ oda/utils/decorators.py
@@ -22,3 +22,16 @@ def sudo_users_only(func: Callable) -> Callable:
             return await func(client, message)
 
     return decorator
+
+def authorized_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+        administrators = await get_administrators(message.chat)
+
+        for administrator in administrators:
+            if administrator == message.from_user.id:
+                return await func(client, message)
+
+    return decorator
